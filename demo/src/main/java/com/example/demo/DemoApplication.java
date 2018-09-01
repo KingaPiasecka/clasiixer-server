397d819174c1dc6bbb1cd86ece5b8136e46a27b9
KingaPiasecka <piasecka.kingaa@gmail.com>
2018-09-01 22:12:19 +0200

    dsadasd

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,15 +7,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
-@Controller
+/*@Controller*/
 @SpringBootApplication
 public class DemoApplication {
 
-	@RequestMapping("/")
+/*	@RequestMapping("/")
 	@ResponseBody
 	String home() {
 		return "Hello World!";
-	}
+	}*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
